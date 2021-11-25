02c7d4b11906b44d6121aed0ea213bb3ad39030c
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-11-25 10:10:56 +0000

    added validators for telephone number  ID and for email

--- src/index.js
+++ src/index.js
@@ -2,67 +2,8 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
-// Questions
-const questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the employee's name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the employee's ID?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the employee's email?",
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "Select job title:",
-    choices: [
-      {
-        name: "Manager",
-        value: "MANAGER",
-      },
-      {
-        name: "Engineer",
-        value: "ENGINEER",
-      },
-      {
-        name: "Intern",
-        value: "INTERN",
-      },
-    ],
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What is the manager's office telephone number?",
-    when: ({ role }) => role === "MANAGER",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is the engineers gitHub profile?",
-    when: ({ role }) => role === "ENGINEER",
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "What is the interns school name?",
-    when: ({ role }) => role === "INTERN",
-  },
-  {
-    type: "confirm",
-    name: "addAnother",
-    message: "Do you wish to add another employee?",
-  },
-];
+const questions = require("./lib/questions");
+const validator = require("email-validator");
 
 const start = async () => {
   let inProgress = true;
---
+++ src/lib/questions.js
@@ -0,0 +1,82 @@
+const validator = require("email-validator");
+
+// Questions
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the employee's ID?",
+    validate: (id) => {
+      if (isNaN(id)) {
+        return "ID should be a number";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the employee's email?",
+    validate: (email) => {
+      if (!validator.validate(email)) {
+        return "Please enter valid email";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "Select job title:",
+    choices: [
+      {
+        name: "Manager",
+        value: "MANAGER",
+      },
+      {
+        name: "Engineer",
+        value: "ENGINEER",
+      },
+      {
+        name: "Intern",
+        value: "INTERN",
+      },
+    ],
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office telephone number?",
+    validate: (officeNumber) => {
+      if (isNaN(officeNumber)) {
+        return "Please enter a valid telephone number";
+      }
+      return true;
+    },
+    when: ({ role }) => role === "MANAGER",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineers gitHub profile?",
+    when: ({ role }) => role === "ENGINEER",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the interns school name?",
+    when: ({ role }) => role === "INTERN",
+  },
+  {
+    type: "confirm",
+    name: "addAnother",
+    message: "Do you wish to add another employee?",
+  },
+];
+
+module.exports = questions;
